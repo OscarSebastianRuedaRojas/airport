c57e0e91e61c407aebd5d298e81fc35b2199a617
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-06-23 18:42:33 -0500

    feat: :construction: DocumentTypes; Created entity.
    Initialized attributes.
    
    @trainingLeader.

---
+++ campusairlines/src/main/java/com/airport/DocumentType/domain/DocumentType.java
@@ -0,0 +1,28 @@
+package com.airport.DocumentType.domain;
+
+/**
+ * DocumentType
+ */
+public class DocumentType {
+    private Long id;
+    private String name;
+    public DocumentType() {
+    }
+    public DocumentType(String name) {
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
