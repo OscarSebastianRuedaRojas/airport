2d2818d06490ce031ed3ead23c64910d3e5a63cc
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-06-23 19:58:00 -0500

    feat: :sparkles: FlightFare: Added Service logic and methods.
    @trainingLeader.

---
+++ campusairlines/src/main/java/com/airport/FlightFare/application/service/FlightFareService.java
@@ -0,0 +1,43 @@
+package com.airport.FlightFare.application.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.airport.FlightFare.application.port.in.IFlightFareService;
+import com.airport.FlightFare.domain.FlightFare;
+import com.airport.FlightFare.infrastructure.adapter.out.FlightFareRepository;
+
+/**
+ * FlightFareService
+ */
+public class FlightFareService implements IFlightFareService {
+    private FlightFareRepository flightFareRepository;
+    
+
+    public FlightFareService(FlightFareRepository flightFareRepository) {
+        this.flightFareRepository = new FlightFareRepository();
+    }
+
+    @Override
+    public FlightFare createFlightFare(FlightFare flightFare) {
+        try {
+            FlightFare createdFlightFare = flightFareRepository.save(flightFare);
+            return createdFlightFare;
+        } catch (Exception e) {
+            System.out.println("Error: Fallo al registrar tarifa.");
+            e.printStackTrace();
+        }
+        return new FlightFare();
+    }
+
+    @Override
+    public List<FlightFare> listFlighFares() { 
+        try {
+            List<FlightFare> flightFares =flightFareRepository.findAll();
+            return flightFares;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+} 
