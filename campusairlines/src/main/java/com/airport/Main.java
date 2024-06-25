54458795ca56cc83a65a411d485761dfcfe4804e
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-06-25 07:34:22 -0500

    feat: :sparkles: Airport: Added attributes, query methods and bussiness logic.

--- campusairlines/src/main/java/com/airport/Main.java
+++ campusairlines/src/main/java/com/airport/Main.java
@@ -1,10 +1,9 @@
 package com.airport;
-import com.airport.City.infrastructure.adapter.in.CityController;
+import com.airport.Airport.infrastructure.adapter.in.AirportController;
 
 public class Main {
     public static void main(String[] args) {
-        CityController troll = new CityController();
-        troll.registerCity();
-        troll.cityList();
+        AirportController troll = new AirportController();
+        troll.updateAirport();
     }
 }
