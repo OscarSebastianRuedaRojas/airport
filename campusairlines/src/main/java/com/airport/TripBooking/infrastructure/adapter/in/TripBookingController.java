da9d430f482db01abd0f687caf807a7e2f06b3e0
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-06-27 07:21:48 -0500

    feat: :sparkles: Controller TripBooking
    Se termino TripBooking Controller con sus respectivas funciones
    
    @trainingLeader

---
+++ campusairlines/src/main/java/com/airport/TripBooking/infrastructure/adapter/in/TripBookingController.java
@@ -0,0 +1,52 @@
+package com.airport.TripBooking.infrastructure.adapter.in;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import com.airport.Trip.infrastructure.adapter.in.TripController;
+import com.airport.TripBooking.application.service.TripBookingService;
+import com.airport.TripBooking.domain.TripBooking;
+
+/**
+ * TripBookingController
+ */
+public class TripBookingController {
+
+    private TripBookingService tripBookingService;
+    private Scanner input;
+
+    public TripBookingController() {
+        this.tripBookingService = new TripBookingService();
+        this.input = new Scanner(System.in);
+    }
+
+    public void registerTripBooking() throws ParseException{
+        try {
+            TripBooking tripBooking = new TripBooking();
+            TripController tripController = new TripController();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            System.out.println("Ingresa la fecha para la reserva: ");
+            String dateStr = input.nextLine();
+            java.util.Date utilDate = dateFormat.parse(dateStr);
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+            tripBooking.setDate(sqlDate);
+            System.out.println("Selecciona el trayecto:");
+            tripBooking.setTrip_id(tripController.listTrips());
+            tripBookingService.save(tripBooking);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public TripBooking findById(Long id){
+        try {
+            return tripBookingService.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+}
