07174d23d8f45c0b421d2d478d62f253f9c50cbc
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-06-28 08:57:11 -0500

    feat :sparkles: Added method.

--- campusairlines/src/main/java/com/airport/TripBooking/infrastructure/adapter/in/TripBookingController.java
+++ campusairlines/src/main/java/com/airport/TripBooking/infrastructure/adapter/in/TripBookingController.java
@@ -21,7 +21,9 @@ public class TripBookingController {
         this.input = new Scanner(System.in);
     }
 
-    public TripBooking registerTripBooking() throws ParseException{
+    public TripBooking 
+    
+    registerTripBooking() throws ParseException{
         try {
             TripBooking tripBooking = new TripBooking();
             TripController tripController = new TripController();
